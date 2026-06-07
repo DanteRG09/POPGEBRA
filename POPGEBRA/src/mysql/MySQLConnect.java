e0dff51ca1b2241f073476287580575cdb626e49
santiago09-code <santiagolopezcruz08@gmail.com>
2026-06-07 02:31:47 -0600

    Inicio de trabajo Santiago

---
+++ POPGEBRA/src/mysql/MySQLConnect.java
@@ -0,0 +1,46 @@
+package mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author santi
+ */
+public class MySQLConnect {
+    
+     // Librería de MySQL
+    public String driver = "com.mysql.cj.jdbc.Driver";
+
+    // Nombre de la base de datos
+    public String database = "databasemovies";
+
+    // Host
+    public String hostname = "localhost";
+
+    // Puerto
+    public String port = "3306";
+
+    // Ruta de nuestra base de datos (desactivamos el uso de SSL con "?useSSL=false")
+    public String url = "jdbc:mysql://" + hostname + ":" + port + "/" + database + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+    // Nombre de usuario
+    public String username = "root";
+
+    // Clave de usuario
+    public String password = "root";
+
+    public Connection conectarMySQL() {
+        Connection conn = null;
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+}
--- POPGEBRA/src/popgebra/POPGEBRA.java
+++ POPGEBRA/src/popgebra/POPGEBRA.java
@@ -4,10 +4,38 @@
 *
 */
 package popgebra;
+
+
+import ConstruccionSwing.CRUD_Registro;
+import mysql.MySQLConnect;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class POPGEBRA {
+    /*public static void main(String[] args)  {
+        
+         MySQLConnect SQL = new MySQLConnect();
+        // Llamas al método que tiene la clase y te devuelve una conexión
+         Connection conn = SQL.conectarMySQL();
+        // Query que usarás para hacer lo que necesites
+        try{
+            String sSQL =   "";
+
+        // Query
+        sSQL =  "INSERT INTO USERS (first_name, last_name) VALUES (?, ?)";
+        // PreparedStatement
+        PreparedStatement pstm = conn.prepareStatement(sSQL);
+        }catch (SQLException e){
+            System.out.println(e); 
+        }
+        
+    }*/
+    
+    
     public static void main(String[] args) {
-        
-        
-        System.out.println("hola soy santi probando");
+        CRUD_Registro registro = new CRUD_Registro();
+        registro.Registro();
     }
+    
 }
